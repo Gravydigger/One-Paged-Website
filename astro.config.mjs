7804e849a379626fbe34fefdb30a9f541e1c1b9f
Gravydigger <gravydigger65@gmail.com>
2023-11-01 17:32:57 +1030

    Upgrade to Astro 3.4.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://gravydigger.net',
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  integrations: []
 });
